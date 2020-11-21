e52684b501893da81b6867673882bface0e70e0d
川鍋清志郎 <53420739+kiyoshirou-kawanabe@users.noreply.github.com>
2020-11-21 16:55:08 +0900

    Update simpson.c

--- simpson.c
+++ simpson.c
@@ -2,7 +2,6 @@
 #include <math.h>
 
 double f(int x){
-<<<<<<< HEAD
   double fx = 0;
   fx = x*x*x*x - 2*x*x*x + x*x - 3*x + 1;
   return fx;
@@ -35,41 +34,4 @@ int main(){
 
   return 0;
 }
-=======
-  double ans = 0;
-  ans = (x*x*x*x) - 2*(x*x*x) + (x*x) - 3*x + 1;
-  return ans;
-}
 
-int main(){
-  int n;
-  printf("台形法を行います.\n積分区間は何等分しますか\n");
-  scanf("%d", &n);
-
-  double y[n+1];
-
-  for(int i = 0; i <= n; i++){
-    int t=0;
-    y[t] = f(i);
-    t++;
-  }
-
-  double s_1 = 0;
-  s_1 = y[0] + y[n];
-  
-  double s_2 = 0;
-  for(int t = 1; t < n; t++){
-    s_2 += y[t];
-  }
-
-
-
-  double s = 0;
-  s = (3 / n * s_1 / 2) + s_2;
-
-  printf("台形法での答えは%fです.\n", s);
-
-return 0;
-}
-
->>>>>>> 11f99da9c135bd8ad2f26d09c7af22e62109a1fb
