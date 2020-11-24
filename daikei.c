c7934e2d7b80a4226352c0b4418bded7d15a429f
Kiyoshiro Kawanabe <s19C1031UL@s.chibakoudai.jp>
2020-11-25 00:15:03 +0900

    daikei_hou

--- daikei.c
+++ daikei.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
-#include <math.h>
 
-double f(int x){
+double f(int x){  //関数定義
   double fx = 0;
   fx = x*x*x*x - 2*x*x*x + x*x - 3*x + 1;
   return fx;
 }
 
-int n, i;
-double delta = 0;
+int n, a;
+float delta, i;
 double s_1 = 0, s_2 = 0, s = 0;
-double y[1000];
+double y[1000];   //yを配列で定義する
 
 int main(){
   printf("台形公式を行います\n積分区間は何等分しますか？\n");
   scanf("%d", &n);
 
-  delta = 3 / n;
+  delta = (double) 3 / n;  //デルタの計算
 
-  for(i = 0; i <= n; i++){
-    y[i] = f(i);
+  for(i = 0, a = 0; a <= n; i = i + delta, a++){  //関数に数字を一つずついれ結果をｙに入れる
+    y[a] = f(i);
   }
 
-  s_1 = (y[0] + y[n]) / 2;
 
-  for(int t = 1; t < n; t++){
+  s_1 = (y[0] + y[n]) / 2;  //y_0,y_nの足し算を2で割る
+
+  for(int t = 1; t < n; t++){  //y_1からy_n-1まで足し算
     s_2 = s_2 + y[t];
   }
+  s = delta * (s_1 + s_2);  //s_1とs_2をたしデルタをかける
 
-  s = delta * (s_1 + s_2);
-
-  printf("答えは%lfです\n", s);
+  printf("答えは%lfです\n", s);  //答えｓを出力
 
   return 0;
 }
+
+
+
 
