a9126ab01fd6df5712b986c817ccea395ed2a184
Kiyoshiro Kawanabe <s19C1031UL@s.chibakoudai.jp>
2021-01-17 21:11:54 +0900

    add coment

--- third_report/monte.c
+++ third_report/monte.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main(){
-  
+
   int max, i;
   printf("ランダムに打つ点の個数を入力してください。");
   scanf("%d", &max);
@@ -12,16 +12,19 @@ int main(){
   int count = 0;
   double x,y,PI;
 
+  /*乱数の種を初期化*/
   srand(time(NULL));
 
+  /*x,yに0∼1の乱数を代入*/
   for(i=0; i<max ; i++){
     x = (double)rand()/RAND_MAX;
     y = (double)rand()/RAND_MAX;
-    
-    if((x*x + y*y) < 1)
+
+    /*円内に入っているか判定*/
+    if((x-0.5)*(x-0.5) + (y-0.5)*(y-0.5) <= 0.25)
       count++;
   }
-
+  /*円周率PIｎの計算*/
   PI = (double)count / max * 4;
   printf("円周率は%f\n", PI);
 
